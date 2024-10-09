31499bbee495f2af5b8f4e12d4fbcce51170f754
errorbyme <amitk74019@gmail.com>
2024-10-09 23:00:28 +0530

    update

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ import path from "path";
 const app = express();
 // Pq3KOOoER9mmKK8d
 mongoose
-  .connect("mongodb+srv://blog-user:Pq3KOOoER9mmKK8d@cluster0.audua.mongodb.net/")
+  .connect(
+    "mongodb+srv://blog-user:Pq3KOOoER9mmKK8d@cluster0.audua.mongodb.net/"
+  )
   .then(() => console.log("Connected to DB"));
 const PORT = process.env.PORT || 9999;
 
@@ -24,6 +26,9 @@ app.use(userRouter);
 app.use(blogRouter);
 app.use(commentRouter);
 
+app.get("/", (req, res) => {
+  res.end("Hey server");
+});
 app.listen(PORT, () => console.log("Server started at", PORT));
 
 // mongodb+srv://blog:HJdsIuc1rIYd5bjq@cluster0.audua.mongodb.net/
